63942866973865296bdcc897323b69007f7162f5
Michael Bradvica <mjbradvica@gmail.com>
2024-03-03 13:48:11 -0500

    fix all

--- src/features/home/HomeView.vue
+++ src/features/home/HomeView.vue
@@ -1,11 +1,5 @@
 <template>
   <p>Home!</p>
-  <p>Current count: {{ counter.count }}</p>
-  <button @click="counter.increment()">Increase</button>
 </template>
 
-<script setup lang="ts">
-import { useCounterStore } from "@/infrastructure";
-
-const counter = useCounterStore();
-</script>
+<script setup lang="ts"></script>
