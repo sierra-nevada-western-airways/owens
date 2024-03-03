1f0b968eea516c6a9d4eed7f1c046064bf80b02f
Michael B <mjbradvica@gmail.com>
2024-03-03 10:10:54 -0500

    husky with stage

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,2 +1,4 @@
 export { default as useState } from "./hooks/use-state/UseState";
 export { default as useId } from "./hooks/use-id/UseId";
+
+export { ValueDefaults } from "./value-defaults/ValueDefaults";
